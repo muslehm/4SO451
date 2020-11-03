0c9877cdd5bdf0d6598f77d4f7aa7493cde802b1
معاذ Mäadh <mm223wa@student.lnu.se>
2020-11-03 10:29:37 +0200

    Add files via upload

--- 4SO451_T_musleh.R
+++ 4SO451_T_musleh.R
@@ -8,18 +8,18 @@ setwd(wd)
 #use your developer account to fill in the values. Go to "Details" in your app. Click on the "keys and tokens" button, copy and paste the values to code below:
 
 twitter_token <- create_token(
-  app = "BLM Ana",
-  consumer_key = "FoLeNTaTU6F3Rb6McDi0nBbve",
-  consumer_secret = "RYlQmr9AhqP7pP7AG0ET4TSujnEtOaFMo4kFCOMn33uVWEQonp",
-  access_token = "1161725350860218369-ZvZL8zMRwdXMxAvBhd4vJQrckbOod7",
-  access_secret = "yrtcQq9UaLqkjckOpQNVDbA3ERWqrOqiI8fFAUaaX6b6W")
+  app = "",
+  consumer_key = "",
+  consumer_secret = "",
+  access_token = "",
+  access_secret = "")
 
 #create a query
 #check https://developer.twitter.com/en/docs/labs/filtered-stream/guides/search-queries for more info on how to build queries that correspond you needs
 query <- "#tal3at OR #طالعات has:mentions"
 
 #full-archive search (up to 5000 tweets per month)
-tweets_s <- search_fullarchive (query, n=5000, env_name = "fanalysis", fromDate = "201909010000", toDate = "201911302359", token = twitter_token)
+tweets_s <- search_fullarchive (query, n=5000, env_name = "", fromDate = "201909010000", toDate = "201911302359", token = twitter_token)
 
 
 all_tweets <- rbind(tweets, tweets_s)
